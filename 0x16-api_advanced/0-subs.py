7bd7351de94d40d59565e1caecdaf22c49f17a9f
kidusshoa <captainkevin2008@gmail.com>
2024-03-07 07:42:46 +0300

    kaskj2

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,42 +1,24 @@
 #!/usr/bin/python3
-"""Write a function that queries the Reddit API and
-   returns the number of subscribers"""
+"""
+Function that queries the Reddit API and returns the number of subscribers
+(not active users, total subscribers) for a given subreddit.
+If an invalid subreddit is given, the function should return 0
+"""
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """A function that returns the number of subscribers for a
-    given subreddit"""
-    user_agent = "0-subs/1.0"
-    headers = {"User-Agent": user_agent}
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    """
+    Function that queries the Reddit API
+    - If not a valid subreddit, return 0.
+    """
+    req = requests.get(
+        "https://www.reddit.com/r/{}/about.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+    )
 
-    try:
-        response = requests.get(url, headers=headers)
-        response.raise_for_status()  # Raise HTTPError for bad responses
-        data = response.json()
-        subscribers = data["data"]["subscribers"]
-        return subscribers
-    except requests.exceptions.HTTPError as http_err:
-        if response.status_code == 404:
-            return 0
-        else:
-            print(f"HTTP error occurred: {http_err}")
-    except requests.exceptions.RequestException as req_err:
-        print(f"Request error occurred: {req_err}")
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
-
-    return 0
-
-
-if __name__ == '__main__':
-    import sys
-
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
+    if req.status_code == 200:
+        return req.json().get("data").get("subscribers")
     else:
-        subreddit = sys.argv[1]
-        subscribers_count = number_of_subscribers(subreddit)
-        print(f"{subscribers_count}")
-
+        return 0
