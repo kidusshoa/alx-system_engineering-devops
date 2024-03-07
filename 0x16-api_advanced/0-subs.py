478e9da59fc5b4577a097110169633ce64dd1e8c
kidusshoa <captainkevin2008@gmail.com>
2024-03-07 17:52:52 +0300

    rjskk

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,26 +1,8 @@
 #!/usr/bin/python3
-
 """
-Reddit Subreddit Subscriber Counter
-
-This script defines a function that queries the Reddit API and returns the number of subscribers
-(total subscribers, not active users) for a given subreddit. If an invalid subreddit is provided,
-the function returns 0.
-
-Dependencies:
-    - requests: Python HTTP library for making API requests
-
-Usage:
-    - Ensure the 'requests' library is installed: pip install requests
-    - Call the 'number_of_subscribers' function with the desired subreddit name.
-
-Example:
-    subscribers_count = number_of_subscribers('python')
-    print(f'The subreddit has {subscribers_count} subscribers.')
-
-Author:
-    Your Name
-
+Function that queries the Reddit API and returns the number of subscribers
+(not active users, total subscribers) for a given subreddit.
+If an invalid subreddit is given, the function should return 0
 """
 
 import requests
@@ -28,13 +10,8 @@ import requests
 
 def number_of_subscribers(subreddit):
     """
-    Query the Reddit API and retrieve the number of subscribers for a given subreddit.
-
-    Args:
-        subreddit (str): The name of the subreddit.
-
-    Returns:
-        int: The total number of subscribers for the subreddit. Returns 0 if the subreddit is invalid.
+    Function that queries the Reddit API
+    - If not a valid subreddit, return 0.
     """
     req = requests.get(
         "https://www.reddit.com/r/{}/about.json".format(subreddit),
